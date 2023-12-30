c4252148e6d984932bd7b717dec98399dbe9283e
omeatai <omeatai@gmail.com>
2023-12-30 11:39:00 -0700

    4. BookList App - Setup CSS in JSX
    4. BookList App - Setup CSS in JSX

--- my-app/src/components/BookList/Author/Author.jsx
+++ my-app/src/components/BookList/Author/Author.jsx
@@ -1,9 +1,10 @@
 const Author = () => {
-  return (
-    <div>
-      <h4>James Clear</h4>
-    </div>
-  );
+  const inlineHeadingStyles = {
+    color: "#e52b50",
+    fontSize: "1rem",
+    marginTop: "0.5rem",
+  };
+  return <h4 style={inlineHeadingStyles}>James Clear</h4>;
 };
 
 export default Author;
