201a814e93f69066964f314b07fc653a2d800d3b
omeatai <omeatai@gmail.com>
2023-12-30 15:45:52 -0700

    8. BookList App - Using Prop Data from List
    8. BookList App - Using Prop Data from List

--- my-app/src/components/BookList/BookList.jsx
+++ my-app/src/components/BookList/BookList.jsx
@@ -1,50 +1,47 @@
 import Book from "./Book/Book";
 
 const BookList = () => {
-  const book = {
-    title:
-      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-    author: "James Clear",
-    img: "./assets/images/atomic_habits.jpg",
-    caption: "Best Selling Book of 2020",
-  };
+  const books = [
+    {
+      author: "James Clear",
+      title:
+        "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+      img: "./assets/images/atomic_habits.jpg",
+      id: 1,
+      caption: "Best Selling Book of 2020",
+    },
+    {
+      author: "Dr. Kimberly D. Moore",
+      title: "The Bible in 52 Weeks: A Yearlong Bible Study for Women",
+      img: "https://m.media-amazon.com/images/I/913KWawg9gL._SL1500_.jpg",
+      id: 2,
+      caption: "Explore how God's wisdom can be applied to your daily life",
+    },
+    {
+      author: "James McBride",
+      title: "The Heaven & Earth Grocery Store: A Novel",
+      img: "https://m.media-amazon.com/images/I/81FS3wyevDL._SL1500_.jpg",
+      id: 3,
+      caption: "A murder mystery locked inside a Great American Novel",
+    },
+    {
+      author: "Liz Cheney",
+      title: "Oath and Honor: A Memoir and a Warning",
+      img: "https://m.media-amazon.com/images/I/71CX11nGhWL._SL1500_.jpg",
+      id: 4,
+      caption:
+        "A gripping first-hand account of the January 6th, 2021, insurrection ",
+    },
+  ];
 
   return (
     <main>
       <h1 className="booklist-heading">Welcome to my BookList</h1>
       <section className="booklist">
-        <Book
-          author={book.author}
-          title={book.title}
-          img={book.img}
-          caption={book.caption}
-        >
-          <p>
-            No matter your goals, Atomic Habits offers a proven framework for
-            improving--every day. James Clear, reveals practical strategies that
-            will teach you exactly how to form good habits, break bad ones, and
-            master the tiny behaviors that lead to remarkable results.
-          </p>
-          <button>Read More...</button>
-        </Book>
-        <Book
-          author={book.author}
-          title={book.title}
-          img={book.img}
-          caption={book.caption}
-        />
-        <Book
-          author={book.author}
-          title={book.title}
-          img={book.img}
-          caption={book.caption}
-        />
-        <Book
-          author={book.author}
-          title={book.title}
-          img={book.img}
-          caption={book.caption}
-        />
+        {books.map((book) => {
+          console.log(book);
+          return <Book {...book} key={book.id} />;
+        })}
       </section>
     </main>
   );
