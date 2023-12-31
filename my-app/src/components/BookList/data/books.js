c0ace5fb7ab6d57cc8d7212dce26466472bc2215
omeatai <omeatai@gmail.com>
2023-12-31 11:24:57 -0700

    13. BookList App - Refactoring with ES6 Modules: Import and Export
    13. BookList App - Refactoring with ES6 Modules: Import and Export

---
+++ my-app/src/components/BookList/data/books.js
@@ -0,0 +1,32 @@
+export const books = [
+  {
+    author: "James Clear",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    img: "./assets/images/atomic_habits.jpg",
+    id: 1,
+    caption: "Best Selling Book of 2020",
+  },
+  {
+    author: "Dr. Kimberly D. Moore",
+    title: "The Bible in 52 Weeks: A Yearlong Bible Study for Women",
+    img: "https://m.media-amazon.com/images/I/913KWawg9gL._SL1500_.jpg",
+    id: 2,
+    caption: "Explore how God's wisdom can be applied to your daily life",
+  },
+  {
+    author: "James McBride",
+    title: "The Heaven & Earth Grocery Store: A Novel",
+    img: "https://m.media-amazon.com/images/I/81FS3wyevDL._SL1500_.jpg",
+    id: 3,
+    caption: "A murder mystery locked inside a Great American Novel",
+  },
+  {
+    author: "Liz Cheney",
+    title: "Oath and Honor: A Memoir and a Warning",
+    img: "https://m.media-amazon.com/images/I/71CX11nGhWL._SL1500_.jpg",
+    id: 4,
+    caption:
+      "A gripping first-hand account of the January 6th, 2021, insurrection ",
+  },
+];
