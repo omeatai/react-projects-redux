53be95a64e7df0c1752eb892f0a035f6ff94ebab
omeatai <omeatai@gmail.com>
2023-12-31 11:44:24 -0700

    14. BookList App - Using Local Images in Src Folder
    14. BookList App - Using Local Images in Src Folder

--- my-app/src/components/BookList/data/books.js
+++ my-app/src/components/BookList/data/books.js
@@ -1,30 +1,35 @@
+import img1 from "../assets/images/book-1.jpg";
+import img2 from "../assets/images/book-2.jpg";
+import img3 from "../assets/images/book-3.jpg";
+import img4 from "../assets/images/book-4.jpg";
+
 export const books = [
   {
     author: "James Clear",
     title:
       "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-    img: "./assets/images/atomic_habits.jpg",
+    img: img1,
     id: 1,
     caption: "Best Selling Book of 2020",
   },
   {
     author: "Dr. Kimberly D. Moore",
     title: "The Bible in 52 Weeks: A Yearlong Bible Study for Women",
-    img: "https://m.media-amazon.com/images/I/913KWawg9gL._SL1500_.jpg",
+    img: img2,
     id: 2,
     caption: "Explore how God's wisdom can be applied to your daily life",
   },
   {
     author: "James McBride",
     title: "The Heaven & Earth Grocery Store: A Novel",
-    img: "https://m.media-amazon.com/images/I/81FS3wyevDL._SL1500_.jpg",
+    img: img3,
     id: 3,
     caption: "A murder mystery locked inside a Great American Novel",
   },
   {
     author: "Liz Cheney",
     title: "Oath and Honor: A Memoir and a Warning",
-    img: "https://m.media-amazon.com/images/I/71CX11nGhWL._SL1500_.jpg",
+    img: img4,
     id: 4,
     caption:
       "A gripping first-hand account of the January 6th, 2021, insurrection ",
