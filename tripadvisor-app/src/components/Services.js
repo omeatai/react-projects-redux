e48bccaeb4966882c2e66ea0373159b7e55381d7
omeatai <omeatai@gmail.com>
2024-01-02 00:27:00 -0700

    22. TripAdvisor App - Setup Title and Services Components
    22. TripAdvisor App - Setup Title and Services Components

--- tripadvisor-app/src/components/Services.js
+++ tripadvisor-app/src/components/Services.js
@@ -1,56 +1,29 @@
+import Title from "./Title";
+import { services } from "../data/data";
+
 const Services = () => {
   return (
     <section className="section services" id="services">
-      <div className="section-title">
-        <h2>
-          our <span>services</span>
-        </h2>
-      </div>
+      <Title title="our" subTitle="services" />
+
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-wallet fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Experience budget-friendly adventures with our exclusive deals and
-              cost-effective travel packages, ensuring you get the most out of
-              your journey without breaking the bank.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-tree fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Discover the thrill of the great outdoors with our diverse range
-              of hiking excursions, from scenic strolls to challenging treks,
-              tailored to cater to all levels of adventurers.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-socks fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Indulge in unparalleled luxury during your travels with our
-              carefully selected accommodations and VIP treatment, ensuring that
-              every moment of your trip is as comfortable as it is
-              unforgettable.
-            </p>
-          </div>
-        </article>
+        {services.map((service) => {
+          const { id, icon, title, text } = service;
+          return (
+            <article className="service" key={id}>
+              <span className="service-icon">
+                <i className={icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{title}</h4>
+                <p className="service-text">{text}</p>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
 };
+
 export default Services;
--- tripadvisor-app/src/data/data.js
+++ tripadvisor-app/src/data/data.js
@@ -11,3 +11,32 @@ export const socialLinks = [
   { id: 3, href: "https://www.instagram.com", icon: "fab fa-instagram" },
   { id: 4, href: "https://www.youtube.com", icon: "fab fa-youtube" },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: `Experience budget-friendly adventures with our exclusive deals and
+              cost-effective travel packages, ensuring you get the most out of
+              your journey without breaking the bank.`,
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: `Discover the thrill of the great outdoors with our diverse range
+              of hiking excursions, from scenic strolls to challenging treks,
+              tailored to cater to all levels of adventurers.
+              `,
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: `Indulge in unparalleled luxury during your travels with our
+              carefully selected accommodations and VIP treatment, ensuring that
+              every moment of your trip is as comfortable as it is
+              unforgettable.`,
+  },
+];
