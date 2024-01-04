4c9f5e1d7c1f15afc16f032ffba22d183a546210
omeatai <omeatai@gmail.com>
2024-01-03 18:00:00 -0700

    28. React Adv - Setup React UseEffect Example
    28. React Adv - Setup React UseEffect Example

--- react-adv/src/App.jsx
+++ react-adv/src/App.jsx
@@ -1,10 +1,11 @@
-import ReactUseStateExample from "./components/ReactUseStateExample";
+// import ReactUseStateExample from "./components/ReactUseStateExample";
+import ReactUseEffectExample from "./components/ReactUseEffectExample";
 
 function App() {
   return (
     <div className="container">
       <h2>React Adv</h2>
-      <ReactUseStateExample />
+      <ReactUseEffectExample />
     </div>
   );
 }
