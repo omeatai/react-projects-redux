4f0cdcdce0b4aab953a6944a5a8bd6b41c1cb4a6
omeatai <omeatai@gmail.com>
2024-01-04 10:02:27 -0700

    30. React Adv - Setup Short Circuit / Tenary Operator
    30. React Adv - Setup Short Circuit / Tenary Operator

--- react-adv/src/App.jsx
+++ react-adv/src/App.jsx
@@ -1,12 +1,13 @@
 // import ReactUseStateExample from "./components/ReactUseStateExample";
 // import ReactUseEffectExample from "./components/ReactUseEffectExample";
-import ReactConditionalRendering from "./components/ReactConditionalRendering";
+// import ReactConditionalRendering from "./components/ReactConditionalRendering";
+import ReactShortCircuitExample from "./components/ReactShortCircuitExample";
 
 function App() {
   return (
     <div className="container">
       <h2>React Adv</h2>
-      <ReactConditionalRendering />
+      <ReactShortCircuitExample />
     </div>
   );
 }
