f1b537f48e4924eba400809f17551e459f6620bb
omeatai <omeatai@gmail.com>
2024-01-04 10:26:29 -0700

    31. React Adv - Setup Cleanup Function for useEffect
    31. React Adv - Setup Cleanup Function for useEffect

--- react-adv/src/App.jsx
+++ react-adv/src/App.jsx
@@ -1,13 +1,14 @@
 // import ReactUseStateExample from "./components/ReactUseStateExample";
 // import ReactUseEffectExample from "./components/ReactUseEffectExample";
 // import ReactConditionalRendering from "./components/ReactConditionalRendering";
-import ReactShortCircuitExample from "./components/ReactShortCircuitExample";
+// import ReactShortCircuitExample from "./components/ReactShortCircuitExample";
+import ReactCleanupFunction from "./components/ReactCleanupFunction";
 
 function App() {
   return (
     <div className="container">
       <h2>React Adv</h2>
-      <ReactShortCircuitExample />
+      <ReactCleanupFunction />
     </div>
   );
 }
