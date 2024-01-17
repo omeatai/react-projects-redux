e6ae4ca9c103d70687577fc0efcb95183aba9ea2
omeatai <omeatai@gmail.com>
2024-01-16 20:35:27 -0700

    38. React Adv - Setup UseRef Example
    38. React Adv - Setup UseRef Example

---
+++ react-adv/src/App.jsx
@@ -0,0 +1,11 @@
+import ReactUseRef from "./components/ReactUseRef";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h2>React Adv</h2>
+      <ReactUseRef />
+    </div>
+  );
+};
+export default App;
